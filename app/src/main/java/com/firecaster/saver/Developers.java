a7b9bd4681885445a54a27f201a88166416d0b16
isaacrojasucr <jorgeisaac.rojas@ucrso.info>
2016-11-17 17:50:45 -0600

    asd

--- app/src/main/java/com/firecaster/saver/Developers.java
+++ app/src/main/java/com/firecaster/saver/Developers.java
@@ -1,5 +1,6 @@
 package com.firecaster.saver;
 
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -7,12 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 /**
  * Created by isaac on 17/11/2016.
  */
-
 public class Developers extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_developers);
+        setContentView(R.layout.activity_devs);
     }
 }
