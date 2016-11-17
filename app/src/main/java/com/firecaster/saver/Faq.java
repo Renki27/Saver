1694dac1544742fa829ea80652b42d3d210d93fc
isaacrojasucr <jorgeisaac.rojas@ucrso.info>
2016-11-17 14:48:15 -0600

    FAQ activity
    sdf

---
+++ app/src/main/java/com/firecaster/saver/Faq.java
@@ -0,0 +1,18 @@
+package com.firecaster.saver;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by isaac on 17/11/2016.
+ */
+
+public class Faq extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+    }
+}
