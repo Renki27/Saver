0b6394ed2a1f6a63078628a579c60c931412e552
isaacrojasucr <jorgeisaac.rojas@ucrso.info>
2016-11-17 13:58:44 -0600

    conect developer

--- app/src/main/java/com/firecaster/saver/Developers.java
+++ app/src/main/java/com/firecaster/saver/Developers.java
@@ -13,6 +13,6 @@ public class Developers extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        setContentView(R.layout.activity_developers);
     }
 }
