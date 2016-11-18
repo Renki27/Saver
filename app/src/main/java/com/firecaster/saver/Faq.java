24ff5dc87c33be1e10f1d73faa7f994b91f49f1b
Renki27 <ejrh27@hotmail.com>
2016-11-17 20:13:15 -0600

    FAKU

--- app/src/main/java/com/firecaster/saver/Faq.java
+++ app/src/main/java/com/firecaster/saver/Faq.java
@@ -1,18 +1,19 @@
 package com.firecaster.saver;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-/**
- * Created by isaac on 17/11/2016.
- */
-
-public class Faq extends AppCompatActivity {
+public class FAQ extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+
+
+        //Instancias que se encargan de agragar el action bar y el boton de regreso a las ventanas
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
