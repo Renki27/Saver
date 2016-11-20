76c7dfbc91a80ffd311c8e49c8fa267ca8a4aa2e
kevsalazararaya <kevin.salazar@ucrso.info>
2016-11-20 11:24:20 -0600

    Licencias y Preguntas Frecuentes
    Kevin

--- app/src/main/java/com/firecaster/saver/Faq.java
+++ app/src/main/java/com/firecaster/saver/Faq.java
@@ -4,7 +4,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FAQ extends AppCompatActivity {
+public class Faq extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
