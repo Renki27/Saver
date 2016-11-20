9ff2536fb9ae46f01a22f09b6283ec430bae7757
isaacrojasucr <jorgeisaac.rojas@ucrso.info>
2016-11-20 11:54:08 -0600

    aaasdas
    asdasda

--- app/src/main/java/com/firecaster/saver/Faq.java
+++ app/src/main/java/com/firecaster/saver/Faq.java
@@ -1,8 +1,12 @@
 package com.firecaster.saver;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+
+/**
+ * Created by isaac on 17/11/2016.
+ */
 
 public class FAQ extends AppCompatActivity {
 
